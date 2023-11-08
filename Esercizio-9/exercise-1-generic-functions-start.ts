c1b8b327fb58d3ab29d493f825b600735d987c7a
udinocera <udinocera@gmail.com>
2023-11-08 14:40:00 +0100

    fine 9 esercizio

--- Esercizio-9/exercise-1-generic-functions-start.ts
+++ Esercizio-9/exercise-1-generic-functions-start.ts
@@ -6,11 +6,12 @@
 // This should fix the type errors on Line 13 and 15.
 // Hint: The type for the `value` function parameter should use a type variable.
 
-function identity(value) {
+function identity<Type>(value: Type): Type {
     return value;
 }
 
 let value1 = identity<string>("France");
+
 
 let value2 = identity<number>(67_413_000);
 
@@ -30,7 +31,7 @@ interface User {
 // Pass the `User` type as a type argument to the generic `fetchData()` function.
 // This should fix the type error on Line 35.
 
-let user = await fetchData("http://api.com/user/1");
+let user = await fetchData<User>("http://api.com/user/1");
 
 console.log(user.name);
 
